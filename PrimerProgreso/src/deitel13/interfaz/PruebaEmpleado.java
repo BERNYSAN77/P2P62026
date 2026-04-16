732862d04ddb965ec9a246df591e1b8c96b8e587
laboratorio <laboratorio@udla.edu.ec>
2026-04-16 10:13:43 -0500

    ejercicio empleado

---
+++ PrimerProgreso/src/deitel13/interfaz/PruebaEmpleado.java
@@ -0,0 +1,21 @@
+package deitel13.interfaz;
+
+import deitel13.negocio.Empleado;
+
+import java.util.Scanner;
+
+public class PruebaEmpleado {
+    public static void main(String[] args) {
+        String nombre, apellido;
+        double salario;
+        double nuevoSalario;
+        Scanner sc = new Scanner(System.in);
+        Empleado em1 = null;
+        Empleado em2 = null;
+
+        System.out.println("El salario anual es: " + em1.getSalario()*12);
+        nuevoSalario = em1.getSalario()*1.10;
+        em1.setSalario(nuevoSalario);
+        System.out.println("El nuevo salario anual es:" + em1.getSalario()*12);
+        em2.setSalario(em2.getSalario()*1.10);
+}
