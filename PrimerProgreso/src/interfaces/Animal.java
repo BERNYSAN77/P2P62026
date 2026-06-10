314e1c99059252cb020dc4bff6f0130b19ac23c5
laboratorio <laboratorio@udla.edu.ec>
2026-06-10 09:07:20 -0500

    interfaces

---
+++ PrimerProgreso/src/interfaces/Animal.java
@@ -0,0 +1,19 @@
+package interfaces;
+
+public abstract class Animal implements IAnimal{
+    private String nombre;
+
+    public Animal(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public abstract String tipoAnimal();
+}
---
+++ PrimerProgreso/src/interfaces/Main.java
@@ -0,0 +1,19 @@
+package interfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<IAnimal> animales = new ArrayList<IAnimal>();
+
+    }
+    public static void main(){
+        System.out.println("1. Agregar gato");
+        System.out.println("2. Agregar perro");
+        System.out.println("3. Mostrar animales");
+        System.out.println("4. Mostrar gatos");
+        System.out.println("5. Mostrar perros");
+        System.out.println("6. Salir");
+    }
+}
